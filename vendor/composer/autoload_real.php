377fcf07689a99d1f9bf6a79a0c0aa74618ff352
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2023-06-16 23:30:59 +0700

    test

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit753e23e3f7b06ccedb48a2196274a38f
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -25,7 +28,7 @@ class ComposerAutoloaderInit753e23e3f7b06ccedb48a2196274a38f
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit753e23e3f7b06ccedb48a2196274a38f::getInitializer($loader));
         } else {
--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -159,7 +159,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-master@082be61bc6b9f9710543b0ba5cfb4968d5889739',
+  'laravel/laravel' => 'dev-main@7daec00b3eab0453612820df3bcd1898fbf647d7',
 );
 
     private function __construct()
