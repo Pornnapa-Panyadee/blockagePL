02b39ccd4d672a6661a55b60755a0365200737e1
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2023-10-01 15:50:40 +0700

    report pdf

--- resources/views/expert/reportpdf_php.php
+++ resources/views/expert/reportpdf_php.php
@@ -525,8 +525,10 @@
                                                 <tr>
                                                     <td align="center"><div style="margin-top:10px;"><img src="<?php echo ($expert[0]->exp_pixmap); ?>" width=140px;></div></td>
                                                     <td align="center"> 
-                                                        <div style="margin-top:5px;"><img src="<?php echo ($expert[0]->exp_pix1); ?>"  width=140px;></div>
-                                                        <div style="margin-top:15px;margin-bottom:10px;"><img src="<?php echo ($expert[0]->exp_pix2); ?> " width=140px;></div>
+                                                        <!-- <div style="margin-top:5px;"><img src="<?php echo ($expert[0]->exp_pix1); ?>"  width=140px;></div>
+                                                        <div style="margin-top:15px;margin-bottom:10px;"><img src="<?php echo ($expert[0]->exp_pix2); ?> " width=140px;></div> -->
+                                                        <div style="margin-top:10px;"><img src="https://watercenter.scmc.cmu.ac.th/blockage/jang_basin/<?php echo($expert[0]->exp_pix1); ?>" width=140px;></div>
+                                                        <div style="margin-top:5px;margin-bottom:5px;"><img src="https://watercenter.scmc.cmu.ac.th/blockage/jang_basin/{<?php echo($expert[0]->exp_pix2); ?>" width=140px;></div>
                                                     </td>
                                                 </tr>
                                         <?php  }else{ ?>
